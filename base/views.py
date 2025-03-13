52270acbbefeb295a7d48368e8f935e1351e413d
TharHtet236401 <tharhteta3@gmail.com>
2025-03-13 18:19:27 +0700

    task add and update

--- base/views.py
+++ base/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView,UpdateView
+from django.urls import reverse_lazy
+
 from .models import Task
 # Create your views here.
 class TaskList(ListView):
@@ -11,5 +14,16 @@ class TaskList(ListView):
 class TaskDetail(DetailView):
     model = Task
     context_object_name = 'task'
-    template_name = 'base/task_detail.html'
+    template_name = 'base/task.html'
    
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
